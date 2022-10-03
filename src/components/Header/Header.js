8132840ed4dd2e3b8d65147758e11f96ed87858e
developer-mdshakil <developerdevelopershakil.cmt1819@gmail.com>
2022-10-03 13:14:12 +0600

    menu items activity show status

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
+    let active = {
+        color: 'red'
+    }
     return (
         <div className='header-area'>
-            <Link className='nav-items' to='/home'>Home</Link>
-            <Link className='nav-items' to='/products'>Products</Link>
-            <Link className='nav-items' to='/users'>Users</Link>
-            <Link className='nav-items' to='/posts'>Posts</Link>
-            <Link className='nav-items' to='/about'>About</Link>
-            <Link className='nav-items' to='/contact'>Contact</Link>
+            <NavLink style={({isActive})=> isActive ? active : undefined} to='/home' 
+            className='nav-items'>Home</NavLink>
+            <NavLink style={({isActive})=> isActive ? active : undefined} className='nav-items' to='/products'>Products</NavLink>
+            <NavLink style={({isActive})=> isActive ? active : undefined} className='nav-items' to='/users'>Users</NavLink>
+            <NavLink style={({isActive})=> isActive ? active : undefined} className='nav-items' to='/posts'>Posts</NavLink>
+            <NavLink style={({isActive})=> isActive ? active : undefined} className='nav-items' to='/about'>About</NavLink>
+            <NavLink style={({isActive})=> isActive ? active : undefined} className='nav-items' to='/contact'>Contact</NavLink>
         </div>
     );
 };
